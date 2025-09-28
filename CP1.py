3495f26b6a4d9fa2f65a9dad30df6859e87158e9
carolccordeiro <carolinafc.scs@gmail.com>
2025-09-28 18:58:52 +0000

    ex 7

--- CP1.py
+++ CP1.py
@@ -61,3 +61,14 @@
 #     print("Seu preço com desconto é: ", preco_final)
 # else:
 #     print("Preço sem desconto é: ", valor)
+
+# 7. Login simples
+# Peça um nome de usuário e uma senha. Se o usuário for "admin" e a senha "1234", exiba “Acesso permitido”; caso contrário, “Acesso negado”.
+
+usuario = input("Digite o nome do usuário: ")
+senha = input("Digite a senha: ")
+if usuario == "admin" and senha == "1234":
+    print("Acesso permitido")
+else:
+    print("Acesso negado")
+

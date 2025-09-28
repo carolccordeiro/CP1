bc404b083406be712e5af1696de4b393f79b0714
carolccordeiro <carolinafc.scs@gmail.com>
2025-09-28 19:20:09 +0000

    ex 10

--- CP1.py
+++ CP1.py
@@ -127,3 +127,21 @@
 # else:
 #     print("Quente")
 
+# 10. Calculadora simples
+
+# Receba dois números inteiros e uma operação (+, -, *, /) digitada pelo usuário. Use if-else para calcular e mostrar o resultado.
+numero_primeiro = int(input("Digite o primeiro número inteiro: "))
+numero_segundo = int(input("Digite o segundo número inteiro: "))
+operacao = input("Digite qual operação gostaria (+, -, *, /): ")
+
+if operacao == "+":
+    print(numero_primeiro + numero_segundo)
+elif operacao == "-":
+    print(numero_primeiro - numero_segundo)
+elif operacao == "*":
+    print(numero_primeiro * numero_segundo)
+elif operacao == "/":
+    print(numero_primeiro / numero_segundo)
+
+else:
+    print("invalido")

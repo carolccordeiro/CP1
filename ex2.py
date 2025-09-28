3f847b29181da7c4fe07fd3d07f1b4edeb77e7de
carolccordeiro <carolinafc.scs@gmail.com>
2025-09-28 19:23:02 +0000

    ex2

---
+++ ex2.py
@@ -0,0 +1,8 @@
+#2. Par ou ímpar
+#Receba um número inteiro e verifique se ele é par ou ímpar.
+
+numero = int(input("Digite um número inteiro: "))
+if numero % 2 == 0:
+    print("par")
+else:
+    print("impar")

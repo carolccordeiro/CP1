d66d1c05c478763e86a508245456a96de9b727b0
carolccordeiro <carolinafc.scs@gmail.com>
2025-09-28 18:23:30 +0000

    ex 3

--- CP1.py
+++ CP1.py
@@ -18,3 +18,12 @@
 # else:
 #     print("impar")
 
+# 3. Maioridade
+# Solicite a idade de uma pessoa e mostre se ela é maior de idade (18 anos ou mais) ou menor de idade.
+
+idade = int(input("Digite a sua idade: "))
+
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")

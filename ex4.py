639506e659824222d1396bac59ff46105af2d2bf
carolccordeiro <carolinafc.scs@gmail.com>
2025-09-28 19:23:50 +0000

    ex4

---
+++ ex4.py
@@ -0,0 +1,9 @@
+# 4. Nota de aprovação
+# Receba a nota de um aluno (0 a 10) e diga se ele foi aprovado (nota ≥ 6) ou reprovado.
+
+nota = int(input("Digite a nota do aluno (0 a 10): "))
+if nota >= 6:
+    print("Aprovado")
+
+else:
+    print("Reprovado")

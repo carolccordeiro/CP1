b20914bd5b35b826ac7bb994af98f14324c8a635
carolccordeiro <carolinafc.scs@gmail.com>
2025-09-28 18:19:59 +0000

    ex 2

--- CP1.py
+++ CP1.py
@@ -1,10 +1,20 @@
 #1. Número positivo ou negativo
 #Peça ao usuário um número inteiro e verifique se ele é positivo, negativo ou zero.
 
-numero = int(input("Digite um número inteiro: "))
-if numero > 0:
-    print("positivo")
-elif numero < 0:
-    print("negativo")
-else:
-    print("Zero")
+# numero = int(input("Digite um número inteiro: "))
+# if numero > 0:
+#     print("positivo")
+# elif numero < 0:
+#     print("negativo")
+# else:
+#     print("Zero")
+
+#2. Par ou ímpar
+#Receba um número inteiro e verifique se ele é par ou ímpar.
+
+# numero = int(input("Digite um número inteiro: "))
+# if numero % 2 == 0:
+#     print("par")
+# else:
+#     print("impar")
+

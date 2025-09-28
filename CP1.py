885f7a7e53a181ae1bac4095df9057f104829b7c
carolccordeiro <carolinafc.scs@gmail.com>
2025-09-28 18:52:44 +0000

    Atualiza exercícios CP1.py até o exercício 6

--- CP1.py
+++ CP1.py
@@ -41,12 +41,23 @@
 # 5. Comparação de dois números
 # Peça dois números inteiros e informe qual deles é maior ou se são iguais.
 
-primeiro = int(input("Digite o primeiro número inteiro: "))
-segundo = int(input("Digite o segundo número inteiro: "))
+# primeiro = int(input("Digite o primeiro número inteiro: "))
+# segundo = int(input("Digite o segundo número inteiro: "))
 
-if primeiro > segundo:
-    print("O primeiro número é maior")
-elif segundo > primeiro:
-    print("O segundo número é maior")
-else:
-    print("Os números são iguais")
+# if primeiro > segundo:
+#     print("O primeiro número é maior")
+# elif segundo > primeiro:
+#     print("O segundo número é maior")
+# else:
+#     print("Os números são iguais")
+
+# 6. Desconto em produto
+# Receba o valor de um produto e mostre o preço final com desconto de 10% se o valor for maior que 100; caso contrário, mostre o preço sem desconto.
+
+# valor = float(input("Digite o valor do produto: R$ "))
+# if valor > 100:
+#     desconto = valor * 0.10
+#     preco_final = valor - desconto
+#     print("Seu preço com desconto é: ", preco_final)
+# else:
+#     print("Preço sem desconto é: ", valor)
